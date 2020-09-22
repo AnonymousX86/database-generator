96697d48b33f31880e946e31b027e09759d98761
Pegux <b.rogus@zsek.olsztyn.pl>
2020-09-22 10:01:49 +0200

    -

--- public/action.php
+++ public/action.php
@@ -1,1 +1,13 @@
 <?php
+
+if(isset($_POST[''])) {
+    $nameMan = file_get_contents("../data/nameMan.json");
+    $nameMan = json_decode($nameMan);
+    $surnameMan = file_get_contents("../data/surnameMan.json");
+    $surnameMan = json_decode($surnameMan);
+    $nameWoman = file_get_contents("../data/nameWoman.json");
+    $nameWoman = json_decode($nameWoman);
+    $surnameWoman = file_get_contents("../data/surnameWoman.json");
+    $surnameWoman = json_decode($surnameWoman);
+
+}
