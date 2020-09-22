4b51f339b8ddfed54e3ec0be0373fcadcdd63248
Jakub <jakub.suchenek.25@gmail.com>
2020-09-22 20:52:11 +0200

    Move JSON data

--- public/action.php
+++ public/action.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST[''])) {
-    $nameMan = file_get_contents("../data/nameMan.json");
+    $nameMan = file_get_contents("data/nameMan.json");
     $nameMan = json_decode($nameMan);
-    $surnameMan = file_get_contents("../data/surnameMan.json");
+    $surnameMan = file_get_contents("data/surnameMan.json");
     $surnameMan = json_decode($surnameMan);
-    $nameWoman = file_get_contents("../data/nameWoman.json");
+    $nameWoman = file_get_contents("data/nameWoman.json");
     $nameWoman = json_decode($nameWoman);
-    $surnameWoman = file_get_contents("../data/surnameWoman.json");
+    $surnameWoman = file_get_contents("data/surnameWoman.json");
     $surnameWoman = json_decode($surnameWoman);
 
 }
